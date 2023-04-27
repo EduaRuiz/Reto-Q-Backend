8bb9395768b8b32eec2268d6dc451936d7bc534b
ERuiz <edwar_775@hotmail.com>
2023-04-27 02:52:03 -0500

    Html para el correo

--- apps/mail-sender-service/src/application/use-cases/send-test-result.use-case.ts
+++ apps/mail-sender-service/src/application/use-cases/send-test-result.use-case.ts
@@ -4,15 +4,63 @@ import { IUseCase } from './interface';
 import { IEventToManage } from '@mail-sender-service/domain/interfaces';
 
 export class SendTestResultUseCase implements IUseCase {
+  private result: number = 0;
+  private readonly template: string = `
+  <!DOCTYPE html>
+<html>
+<head>
+	<title>Test Result</title>
+	<style>
+		.highlight {
+			font-size: 48px;
+			font-weight: bold;
+		}
+	</style>
+</head>
+<body style="margin: 0; padding: 0;">
+	<table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="background-color: #012d31; font-family: Arial, sans-serif;">
+		<tr>
+			<td align="center" style="padding: 50px;">
+				<table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="max-width: 600px; background-color: #13ffd8; color: #012d31; border-radius: 10px; box-shadow: 0px 0px 20px rgba(0,0,0,0.5);">
+					<tr>
+						<td align="center" style="padding: 30px 20px 10px 20px;">
+							<h2 style="font-size: 24px; font-weight: bold; margin: 0;">Hello there!</h2>
+						</td>
+					</tr>
+					<tr>
+						<td align="center" style="padding: 10px 20px 30px 20px;">
+							<p style="font-size: 24px; font-weight: bold; margin: 0;">Here your test result is:</p>
+							<p style="font-size: 48px; margin: 40px 0 0 0;">
+								<span>{this.result}</span>
+								<span>/</span>
+								<span class="highlight">30</span>
+							</p>
+							<p style="font-size: 16px; margin: 40px 0 0 0; text-align: center;">
+								<span style="font-weight: bold;">Congratulations!</span>
+								<span style="display: block;">Your score is greater than 26, and you have advanced to a new level.</span>
+								<span style="display: block;">You can now generate a new token and take a new exam.</span>
+							</p>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
+`;
+
   constructor(private readonly emailService: IEmailDomainService) {}
+
   execute(data: IEventToManage): Observable<string> {
-    const result = data.test.questions.reduce((acc, question) => {
+    this.result = data.test.questions.reduce((acc, question) => {
       return acc + question.points;
     }, 0);
     return this.emailService.sendEmail(
       data.userEmail,
       'Test result',
-      `Hi, your test result is ${result}/30`,
+      `Hi, your test result is ${this.result}/30`,
+      this.template.replace('{this.result}', this.result.toString()),
     );
   }
 }
--- apps/mail-sender-service/src/application/use-cases/send-token.use-case.ts
+++ apps/mail-sender-service/src/application/use-cases/send-token.use-case.ts
@@ -4,12 +4,46 @@ import { Observable } from 'rxjs';
 import { IEventToManage } from '@mail-sender-service/domain/interfaces';
 
 export class SendTokenUseCase implements IUseCase {
+  private token: string = '';
+  private readonly template: string = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>Token centrado</title>
+  </head>
+  <body style="margin: 0; padding: 0;">
+    <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="background-color: #012d31; font-family: Arial, sans-serif;">
+      <tr>
+        <td align="center" style="padding: 50px;">
+          <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="max-width: 600px; background-color: #13ffd8; color: #012d31; border-radius: 10px; box-shadow: 0px 0px 20px rgba(0,0,0,0.5);">
+            <tr>
+              <td align="center" style="padding: 30px 20px 10px 20px;">
+                <h2 style="font-size: 24px; font-weight: bold; margin: 0; text-transform: uppercase;">Hello there!</h2>
+              </td>
+            </tr>
+            <tr>
+              <td align="center" style="padding: 10px 20px 30px 20px;">
+                <h1 style="font-size: 80px; font-weight: bold; margin: 0; text-transform: uppercase;">{this.token}</h1>
+                <p style="font-size: 24px; font-weight: bold; margin: 0; text-transform: uppercase;">Your token is ready</p>
+                <p style="font-size: 16px; margin: 40px 0 0 0;">Please note that your token is only valid for 24 hours. If you don't take the exam within this period, you will not have any other opportunities.</p>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+  </body>
+  </html>
+`;
   constructor(private readonly emailService: IEmailDomainService) {}
+
   execute(data: IEventToManage): Observable<string> {
+    this.token = data.test.token;
     return this.emailService.sendEmail(
       data.userEmail,
       'Token to present test',
       `Hi, your token is ${data.test.token}`,
+      this.template.replace('{this.token}', this.token),
     );
   }
 }
